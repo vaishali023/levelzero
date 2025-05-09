76de1a2298a3fdc572a56a5191df381a58a98066
Vaishali Chaudhary <vaishalichaudhary011@gmail.com>
2025-05-09 12:43:13 -0400

    Refactor: Update Token hooks

--- src/hooks/useTokens.ts
+++ src/hooks/useTokens.ts
@@ -1,29 +1,28 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from "react"
 
 export type Token = {
-    symbol:string
-    name: string
-    address: string
-    logoURI: string
+  symbol: string
+  name: string
+  address: string
+  logoURI: string
 }
 
-export function useTokens(chainId: number = 1) {
-   const [tokens, setTokens] = useState<Token[]>([]);
-   const[loading, setLoading] = useState(true);
+export function useTokens() {
+  const [tokens, setTokens] = useState<Token[]>([])
+  const [loading, setLoading] = useState(true)
 
-   useEffect(() => {
-    setLoading(true)
-    fetch(`https://tokens.1inch.io/v1.1/${chainId}`)
-    .then((res) => res.json())
-    .then((data) => {
-        setTokens(Object.values(data))
-    })
-    .catch((err) => {
-        console.error("Error fetching tokens: ", err)
-        setTokens([])
-    })
-    .finally(() => setLoading(false))
-   },[chainId])
+  useEffect(() => {
+    setTokens([
 
-    return {tokens, loading};
+      {
+        symbol: "AO",
+        name: "AO Token",
+        address: "arweave:ao",
+        logoURI: "/logos/ao.svg",
+      },
+    ])
+    setLoading(false)
+  }, [])
+
+  return { tokens, loading }
 }
